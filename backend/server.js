02b607cecb2ea288442de61d7a507a3cc7add400
paulsunny90 <paulsunny87267@gmail.com>
2026-03-06 12:57:11 +0530

    product

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,17 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import connectdb from './config/db.js'
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 
 
 const app =express()
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", productRoutes);
 
 const PORT = process.env.PORT
 
